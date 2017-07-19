44612dcbf738493cceb4c886daf16a1cd4923ccb
Simran Brucherseifer <simran@arangodb.com>
2017-07-19 20:35:19 +0200

    Add v3.2 and fix 2.8 cookbook switching

--- recipes/styles/header.js
+++ recipes/styles/header.js
@@ -31,6 +31,7 @@ function appendHeader() {
     '  </div>\n' +
     '  <select class="arangodb-version-switcher">\n' +
     '    <option value="devel">devel</option>\n' +
+    '    <option value="3.2">v3.2</option>\n' +
     '    <option value="3.1">v3.1</option>\n' +
     '    <option value="3.0">v3.0</option>\n' +
     '    <option value="2.8">v2.8</option>\n' +
@@ -107,7 +108,9 @@ function appendHeader() {
     e.preventDefault();
     var urlSplit = gitbook.state.root.split("/");
     urlSplit.pop(); // ""
-    urlSplit.pop(); // e.g. "Manual"
+    if (urlSplit.slice(-1) == "cookbook" && gitbook.state.config.title.indexOf("v2.8") != -1) {
+      urlSplit.pop();
+    }
     window.location.href = urlSplit.join("/") + "/" + e.target.getAttribute("data-book") + "/index.html";
   });
 
